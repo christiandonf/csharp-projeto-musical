12679a5da0e9a7bdfff59b9d1cc4abb540606cfc
Christian <87277467+christiandonf@users.noreply.github.com>
2025-03-04 17:06:38 -0300

    Create logo, Created Interactive Menu and Created List of Bands

--- Program.cs
+++ Program.cs
@@ -1,4 +1,93 @@
 ﻿// Screen Sound
-string mensagemDeBoasVindas = "Boas Vindas ao Screen Sound";
+using System.Xml;
 
-Console.WriteLine(mensagemDeBoasVindas);
+string mensagemDeBoasVindas = "Boas Vindas ao Screen Sound";
+List<string> listaDasBandas = new List<string>{"U2","The Beatles","Linkin park","Pink Floyd","Calypso"};
+//string curso = "Screen Sound";
+//string nome1 = "Daniel";
+//string nome2 = "Guilherme";
+//string sobrenome1 = " Portugal";
+//string sobrenome2 = " Lima";
+void ExibirLogo()
+{
+    Console.WriteLine(@"
+    ░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+    ██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+    ╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+    ░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+    ██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+    ╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+    ");
+    Console.WriteLine(mensagemDeBoasVindas);
+    //Console.WriteLine(curso);
+    //Console.WriteLine(nome1+sobrenome1);
+    //Console.WriteLine(nome2+sobrenome2+"\n");
+}
+void ExibirOpcoesDoMenu()
+{
+    ExibirLogo();
+    Console.WriteLine("Digite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1:
+            RegistrarBanda();
+            break;
+        case 2:
+            MostrarBandasRegistradas();
+            break;
+        case 3:
+            Console.WriteLine("Você digitou a opção " + opcaoEscolhida);
+            break;
+        case 4:
+            Console.WriteLine("Você digitou a opção " + opcaoEscolhida);
+            break;
+        case -1:
+            Console.WriteLine("Falow ae :)");
+            break;
+        default:
+            Console.WriteLine("Opção Inválida");
+            break;
+    }
+}
+void RegistrarBanda()
+{
+    Console.Clear();
+    Console.WriteLine("*********************************");
+    Console.WriteLine("Registro de bandas");
+    Console.WriteLine("*********************************\n");
+    Console.Write("Digite o nome da banda que deseja registrar: ");
+    string nomeDaBanda = Console.ReadLine()!;
+    listaDasBandas.Add(nomeDaBanda);
+    Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
+    Thread.Sleep(2000);
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+}
+void MostrarBandasRegistradas()
+{
+    Console.Clear();
+    Console.WriteLine("*********************************");
+    Console.WriteLine("Exibindo as bandas registradas");
+    Console.WriteLine("*********************************\n");
+    // for (int i =0; i < listaDasBandas.Count; i++)
+    // {
+    //     Console.WriteLine($"Banda: {listaDasBandas[i]}");
+    // }
+    foreach(string banda in listaDasBandas)
+    {
+        Console.WriteLine($"Banda: {banda}");
+    }
+    Console.WriteLine("\nDigite uma tecla para volta ao menu principal");
+    Console.ReadKey();
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+}
+ExibirOpcoesDoMenu();
--- obj/Debug/net9.0/csharp-projeto-musical.AssemblyInfo.cs
+++ obj/Debug/net9.0/csharp-projeto-musical.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp-projeto-musical")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f47bb82fafd1bde594a11e113119b570c567c11c")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp-projeto-musical")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp-projeto-musical")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
